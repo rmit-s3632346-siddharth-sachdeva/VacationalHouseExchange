9e47ef0fb008453fe4eb872f51622f78c621e40c
rmit-s3632346-siddharth-sachdeva <s3632346@student.rmit.edu.au>
2017-04-08 18:35:15 +1000

    Written code for Junit Testing framework, added test cases for login service, also added model layer for business logic.

---
+++ src/model/LoginModel.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import database.DBOperations;
+
+public class LoginModel {
+	String statusCode = "1";
+	DBOperations dbOperations = new DBOperations();
+	String response = null;
+	
+	public String loginModel(String username, String password){
+		try{
+		String sqlQuery = "SELECT 0 FROM User WHERE username='" + username + "' AND password='" + password + "';";
+		ResultSet resultSet = dbOperations.getData(sqlQuery);
+
+		while (resultSet.next()) {
+			response = resultSet.getString("0");
+		}
+
+		if (response != null && response.equalsIgnoreCase("0")) {
+			statusCode = "0";
+		} else {
+			statusCode = "1";
+		}
+		}catch(SQLException se){
+			se.printStackTrace();
+		}
+		return statusCode;
+	}
+
+}
--- src/webservices/LoginService.java
+++ src/webservices/LoginService.java
@@ -1,53 +1,33 @@
 package webservices;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import javax.ws.rs.POST;
 import javax.ws.rs.core.Response;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import database.DBOperations;
+import model.LoginModel;
 
 public class LoginService {
+
 	String statusCode = "1";
-	DBOperations dbOperations = new DBOperations();
+	LoginModel loginModel = new LoginModel();
+	
 	@POST
-	public Response login(String request) throws ParseException{
-		
-		JSONObject requestObject = new JSONObject();
-		JSONParser parser = new JSONParser();
-		requestObject = (JSONObject) parser.parse(request);
-		String response = null;
-		String username = requestObject.get("username").toString();
-		String password = requestObject.get("password").toString();
-		
-		String sqlQuery = "SELECT 0 FROM User WHERE username='"+username+"' AND password='"+password+"';";
-		ResultSet resultSet = dbOperations.getData(sqlQuery);
+	public Response login(String request) {
 		try {
-			while(resultSet.next()){
-			response = resultSet.getString("0");
-			}
-			
-			if(response.equalsIgnoreCase("0")){
-				statusCode = "0";
-			}else{
-				statusCode = "1";
-			}
-			
-		} catch (SQLException e) {
+			JSONObject requestObject = new JSONObject();
+			JSONParser parser = new JSONParser();
+			requestObject = (JSONObject) parser.parse(request);
+			String username = requestObject.get("username").toString();
+			String password = requestObject.get("password").toString();
+			statusCode = loginModel.loginModel(username, password);
+		} catch (Exception e) {
 			e.printStackTrace();
 			statusCode = "1";
 		}
-		//System.out.println("im inside:"+test);
-		//return statusCode;
-		return Response.ok() //200
-				.entity(statusCode)
-				.header("Access-Control-Allow-Origin", "*")
-				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
-				.allow("OPTIONS").build();
+
+		return Response.ok().entity(statusCode).header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").allow("OPTIONS").build();
 	}
 }
