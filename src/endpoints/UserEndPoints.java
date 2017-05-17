b260bd902324c310f6f06064d203476b1c131d50
rmit-s3632346-siddharth-sachdeva <s3632346@student.rmit.edu.au>
2017-05-17 19:22:15 +1000

    Webservice changes

--- src/endpoints/UserEndPoints.java
+++ src/endpoints/UserEndPoints.java
@@ -10,31 +10,30 @@ import webservices.RegisterService;
 
 @Path("/userendpoints")
 public class UserEndPoints {
-	
+
 	@Path("/login")
 	public LoginService loginEndPoint() {
 		return new LoginService();
 	}
-	
+
 	@Path("/register")
 	public RegisterService registerEndPoint() {
 		return new RegisterService();
 	}
-	
+
 	@Path("/editHouseDetails")
 	public EditHouseDetailsService editHouseDetails() {
 		return new EditHouseDetailsService();
 	}
-	
-<<<<<<< HEAD
+
 	@Path("/logout")
 	public LogoutService logoutEndPoint() {
 		return new LogoutService();
-=======
+	}
+
 	@Path("/forgetPassoword")
 	public ForgetPassowordService forgetPassoword() {
 		return new ForgetPassowordService();
->>>>>>> a7b9c6d91d0f7ef0a0c03d60e4b6e13338b09c89
 	}
-	
+
 }
--- src/model/AcceptHouseRequestModel.java
+++ src/model/AcceptHouseRequestModel.java
@@ -3,6 +3,8 @@ package model;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.RegularExpression;
+
 import common.Constants;
 import database.DBOperations;
 
@@ -11,11 +13,21 @@ public class AcceptHouseRequestModel {
 	private String statusCode = "1";
 	private DBOperations dbOperations = new DBOperations();
 	
-	public String acceptHouseRequest(String requesterEmailId, String ownerEmailID) throws SQLException {
+	public String acceptHouseRequest(String ownerEmailId) throws SQLException {
+		
+		
+		String fetchRequesterEmailIdSQLQuery = "SELECT requesterEmailId From Book_House WHERE ownerEmailId ='"+ownerEmailId+"';"; 
+		
+		ResultSet resultSet = dbOperations.getData(fetchRequesterEmailIdSQLQuery);
+		String requesterEmailId = null;
+		
+		while (resultSet.next()) {
+			requesterEmailId = resultSet.getString(Constants.requesterEmailId);
+		}
 		
 		String fetchRequesterCreditsSQL = "SELECT credits FROM User WHERE email_id='"+requesterEmailId+"'";
-		String fetchOwnerCreditsSQL = "SELECT credits FROM User WHERE email_id='"+ownerEmailID+"'";
-		String fetchOwnerHousePointsRequiredSQL = "SELECT points_required FROM User WHERE email_id='"+ownerEmailID+"'";
+		String fetchOwnerCreditsSQL = "SELECT credits FROM User WHERE email_id='"+ownerEmailId+"'";
+		String fetchOwnerHousePointsRequiredSQL = "SELECT points_required FROM User WHERE email_id='"+ownerEmailId+"'";
 		String requesterCredits = null;
 		String ownerCredits = null;
 		String ownerHousePointsRequired = null;
@@ -37,8 +49,8 @@ public class AcceptHouseRequestModel {
 		
 		
 		String updateRequesterCreditsSQL ="UPDATE User SET credits ='"+(Integer.parseInt(requesterCredits) - Integer.parseInt(ownerHousePointsRequired)+"' WHERE email_id ='"+requesterEmailId+"'");
-		String updateOwnerCreditsSQL = "UPDATE User SET credits ='"+(Integer.parseInt(ownerCredits) + Integer.parseInt(ownerHousePointsRequired)+"' WHERE email_id ='"+ownerEmailID+"'");
-		String updateHouseStatus = "UPDATE House_Details SET status = 'booked' WHERE email_id ='"+ownerEmailID+"'";
+		String updateOwnerCreditsSQL = "UPDATE User SET credits ='"+(Integer.parseInt(ownerCredits) + Integer.parseInt(ownerHousePointsRequired)+"' WHERE email_id ='"+ownerEmailId+"'");
+		String updateHouseStatus = "UPDATE House_Details SET status = 'booked' WHERE email_id ='"+ownerEmailId+"'";
 		
 		dbOperations.updateData(updateRequesterCreditsSQL);
 		dbOperations.updateData(updateOwnerCreditsSQL);
